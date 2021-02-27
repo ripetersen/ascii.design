07e52a388fa8025db440f375bced4952890a1b04
Robert Petersen <robert@orangefood.com>
2021-02-27 12:19:03 -0800

    lint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
